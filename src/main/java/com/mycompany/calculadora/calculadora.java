18bb8f98e941d4b0dd1f41237e753ee6477d5bbf
joseluis87 <jlaa1@hotmail.com>
2022-02-12 00:17:44 +0000

    subiendo las operaciones matematicas

--- src/main/java/com/mycompany/calculadora/calculadora.java
+++ src/main/java/com/mycompany/calculadora/calculadora.java
@@ -10,4 +10,22 @@ package com.mycompany.calculadora;
  */
 public class calculadora {
     
+    public static int suma(int a, int b)
+            
+    {
+        return a+b;
+    }
+    
+        public static int resta(int a, int b)
+            
+    {
+        return a-b;
+    }
+    
+        
+       public static void main(String[] args) {		
+		System.out.println(" la Suma de 10+5 es " + suma(10,5));
+                System.out.println(" la Suma de 10-5 es " + resta(10,5));
+	} 
+    
 }
